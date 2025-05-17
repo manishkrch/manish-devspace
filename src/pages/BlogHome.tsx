bda81fee7bf6eb6678f1ad8d71f4e3f83ef80c6b
Manish <manishki@live.com>
2025-05-18 00:22:26 +0530

    Remove duplicate subescribe buttion

--- src/pages/BlogHome.tsx
+++ src/pages/BlogHome.tsx
@@ -93,7 +93,6 @@ const BlogHome: React.FC = () => (
             </div>
           </div>
         ))}
-        <Subscribe />
         <nav className="blog-nav nav nav-justified my-5">
           <a className="nav-link-prev nav-item nav-link d-none rounded-left" href="#">Previous<i className="arrow-prev fas fa-long-arrow-alt-left"></i></a>
           <a className="nav-link-next nav-item nav-link rounded" href="#">Next<i className="arrow-next fas fa-long-arrow-alt-right"></i></a>
